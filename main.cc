5f0e9779403bdb18a486c50b9e960a099492cdff
sksat <sksat@sksat.net>
2019-09-14 19:37:43 +0900

    [add] impl inc(reg),dec(reg)

--- main.cc
+++ main.cc
@@ -21,6 +21,16 @@ int main(int argc, char **argv){
 			<< "opcode = " << (uint32_t)opcode << std::endl;
 
 		switch(opcode){
+			// inc(reg)
+			case 0x40: case 0x41: case 0x42: case 0x43:
+			case 0x44: case 0x45: case 0x46: case 0x47:
+				reg[opcode - 0x40]++;
+				break;
+			// dec(reg)
+			case 0x48: case 0x49: case 0x4a: case 0x4b:
+			case 0x4c: case 0x4d: case 0x4e: case 0x4f:
+				reg[opcode - 0x48]--;
+				break;
 			case 0x90: // NOP
 				break;
 			case 0xf4: // HLT
